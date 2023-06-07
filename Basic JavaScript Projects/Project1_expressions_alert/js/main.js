018cd12ec2c25d03b977f5e87b2d29435e967632
jbikeler <jbikeler@gmail.com>
2023-06-07 12:01:12 -0700

    First Embeded Script

---
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,7 @@
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string";
+
+window.alert("This is the a test alert!");
+
+document.write(Sent1 + Sent2); //I believe this line acomplishes both a concatination and an expression
+
+
