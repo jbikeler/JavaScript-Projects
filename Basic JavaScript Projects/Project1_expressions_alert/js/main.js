d8318206692a47b33ec651a2f2ceb8266d51d2cd
jbikeler <jbikeler@gmail.com>
2023-06-07 12:05:57 -0700

    Update comments

--- Basic JavaScript Projects/Project1_expressions_alert/js/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -1,7 +1,7 @@
 var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string";
 
-window.alert("This is the a test alert!");
+window.alert("This is the a test alert!"); //Popup alert when opening page
 
-document.write(Sent1 + Sent2); //I believe this line acomplishes both a concatination and an expression
+document.write(Sent1 + Sent2); //I believe this line acomplishes both a concatenation and an expression
 
 
